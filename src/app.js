06c7d7aaf2d5bf84c6171d8454279a82553f7162
Cauã <caua.schmitt@hotmail.com.br>
2023-11-24 12:19:07 -0300

    fix

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const router = require('./routes/rotas');
+
+app.set('port', process.env.PORT || 3333);
+app.use('/api', router);
+
+module.exports = app;
